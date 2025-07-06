fc958889559316610bd09b02ea713f307e675390
Elis-Vasconcelos <elisolivasco@gmail.com>
2025-07-06 18:40:19 -0300

    feat: update Usuario class to manage emprestimos and reservas

--- Usuario.java
+++ Usuario.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 
 public class Usuario {
     String codigo;
     String nome;
     int tempoEmprestimo;
+    ArrayList<Emprestimo> emprestimos = new ArrayList<>();
+    ArrayList<Reserva> reservas = new ArrayList<>();
 
     public Usuario(String codigo,String nome) {
         this.codigo = codigo;
@@ -31,5 +34,21 @@ public class Usuario {
 
     public void setTempoEmprestimo(int tempoEmprestimo) {
         this.tempoEmprestimo = tempoEmprestimo;
+    }
+
+    public ArrayList<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(ArrayList<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    public ArrayList<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(ArrayList<Reserva> reservas) {
+        this.reservas = reservas;
     }
 }
