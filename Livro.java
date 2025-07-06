96968316ea7f57e06d4af880ebefd548e99b35a8
Esther <estherrodrigues001@gmail.com>
2025-07-06 18:39:05 -0300

    classes básicas

--- Livro.java
+++ Livro.java
@@ -1,4 +1,75 @@
 
-public class Livro {
+import java.util.ArrayList;
 
+
+class Livro {
+
+    String codigo, titulo, editora, edição;
+    ArrayList<String> autores = new ArrayList<>();
+    ArrayList<Exemplar> exemplares = new ArrayList<>();
+    ArrayList<Reservas> reservas = new ArrayList<>();
+    int ano;
+
+    public Livro (String codigo, String titulo, String editora, String edição, String[] autores, int ano ){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.editora = editora;
+        this.edição = edição;
+        this.ano = ano;
+
+        for (String autor : autores ){
+            this.autores.add(autor);
+        }
+
+        
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getEdição() {
+        return edição;
+    }
+
+    public void setEdição(String edição) {
+        this.edição = edição;
+    }
+
+    public ArrayList<String> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(ArrayList<String> autores) {
+        this.autores = autores;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
 }
