842056c327e959ba8fa86c074c56a6687e57e179
Elis-Vasconcelos <elisolivasco@gmail.com>
2025-07-07 00:37:01 -0300

    feat: regras de emprestimo implementadas

--- RegraAluno.java
+++ RegraAluno.java
@@ -5,24 +5,41 @@ public class RegraAluno implements IRegraEmprestimo {
         this.limiteLivros = limiteLivros;
     }
     
-
+    // Vale a pena separar em dois tipos de classe? Validador de emprestimo e emprestimo
     @Override
     public Emprestimo emprestar(Usuario usuario, Livro livro) {
         if (usuario.isDevendo()) {
+            MensagensEmprestimo.MensagemUsuarioEstaDevendo();
+            return null;
+            
+        } else if (livro.getQtdExemplaresDisponiveis() == 0) {
+            MensagensEmprestimo.MensagemEmprestimoSemExemplar();
             return null;
 
         } else if (usuario.getQtdEmprestimosAbertos() >= limiteLivros){
+            MensagensEmprestimo.MensagemEmprestimoMaximo();
             return null;
 
         } else if (livro.getQtdReservas() >= livro.getQtdExemplaresDisponiveis() && !(usuario.temLivroReservado(livro.getCodigo()))) {
+            MensagensEmprestimo.MensagemSemReserva();
             return null;
 
         } else if (usuario.temLivroEmprestado(livro.getCodigo())) {
+            MensagensEmprestimo.MensagemEmprestimoJaTemEmprestado();
             return null;
+        
+        }  
 
-        }
 
-        return new Emprestimo(livro.getTitulo(), usuario, livro.buscarAtualizarExemplar());
+
+        Exemplar exemplarDisponivel = livro.buscarAtualizarExemplar();
+        Emprestimo emprestimo = new Emprestimo(livro.getTitulo(), usuario, exemplarDisponivel);
+        
+        usuario.adicionarEmprestimo(emprestimo);
+        exemplarDisponivel.adicionarEmprestimo(emprestimo);
+        
+        return emprestimo;
+        
 
     } 
 }
